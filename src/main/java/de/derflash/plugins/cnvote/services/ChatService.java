972c1b7751d420072df73a56606d73a09f8f4ff0
twonky <twonky4@gmx.de>
2013-03-17 15:49:36 +0100

    change chat color

--- src/main/java/de/derflash/plugins/cnvote/services/ChatService.java
+++ src/main/java/de/derflash/plugins/cnvote/services/ChatService.java
@@ -6,22 +6,22 @@ import org.bukkit.entity.Player;
 
 public class ChatService {
     private static final String CHAT_NAME = "Vote";
-    private static final ChatColor COLOR_DARK = ChatColor.YELLOW;
+    private static final ChatColor COLOR_DARK = ChatColor.BLUE;
     private static final ChatColor COLOR_LIGHT = ChatColor.WHITE;
 
-    public void broadcastVote(String username) {
+    public void broadcastVote(String playerName) {
         Bukkit.getServer().broadcastMessage(
-                COLOR_DARK + "[" + CHAT_NAME + "]" + COLOR_LIGHT + " " + username
+                COLOR_DARK + "[" + CHAT_NAME + "]" + COLOR_LIGHT + " " + playerName
                         + " hat den Server gerade mit einem Vote unterstützt. Klickt hier und helft mit:" + COLOR_DARK + " cube-nation.de/danke");
     }
 
     public void showPayedIntoInventory(Player player, String service, int amount) {
-        player.sendMessage(ChatColor.AQUA + "[" + CHAT_NAME + "]" + ChatColor.WHITE + " Danke, dass du auf " + service
+        player.sendMessage(COLOR_DARK + "[" + CHAT_NAME + "]" + ChatColor.WHITE + " Danke, dass du auf " + service
                 + " gevotet hast! Als kleines Dankeschön wurden dir " + amount + " Smaragde in dein Invenvar gelegt.");
     }
 
     public void showPayedIntoBank(Player player, String service, int amount) {
-        player.sendMessage(ChatColor.AQUA + "[" + CHAT_NAME + "]" + ChatColor.WHITE + " Danke, dass du auf " + service
+        player.sendMessage(COLOR_DARK + "[" + CHAT_NAME + "]" + ChatColor.WHITE + " Danke, dass du auf " + service
                 + " gevotet hast! Als kleines Dankeschön wurden dir " + amount + " Smaragde auf deinem Bankkonto gutgeschrieben.");
     }
 }
